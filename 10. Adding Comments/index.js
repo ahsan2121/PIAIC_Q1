f9c98f9ca191e41a38e46c26fabac3b9ca047071
Rana Ahsan Ansar <asn.cs21@gmail.com>
2023-02-13 17:27:04 +0500

    Q10 done

---
+++ 10. Adding Comments/index.js
@@ -0,0 +1,9 @@
+// Program that stores and displays a favorite number
+var favoriteNumber = 7;
+var message = "My favorite number is " + favoriteNumber + ".";
+console.log(message);
+// Program that display a quote with author name 
+var quote2 = "The wing of the Falcon brings to the king, the wing if the crow brings him to the cemetery.";
+var famous_person2 = "Muhammad Iqbal";
+var message2 = "".concat(famous_person2, " once said, \"").concat(quote2, "\"");
+console.log(message2);
---
+++ 10. Adding Comments/index.ts
@@ -0,0 +1,13 @@
+// Program that stores and displays a favorite number
+let favoriteNumber: number = 7;
+let message: string = "My favorite number is " + favoriteNumber + ".";
+
+console.log(message);
+
+// Program that display a quote with author name 
+let quote2: string = "The wing of the Falcon brings to the king, the wing if the crow brings him to the cemetery.";
+let famous_person2: string = "Muhammad Iqbal";
+
+let message2: string = `${ famous_person2 } once said, "${quote2}"`;
+
+console.log(message2);
